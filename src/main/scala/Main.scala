92a53a6145cc31290297d88259d3ff3ee3a1e5f7
pii5656 <microapple56@gmail.com>
2016-02-06 02:52:04 +0900

    post-every-10minutes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 import collection.JavaConverters._
+import com.flyberrycapital.slack.SlackClient
 import org.jsoup._
 import org.jsoup.nodes._
 import scala.util.matching._
@@ -6,31 +7,39 @@ import scala.util.matching._
 object Main {
   var max_id = 0
   val baseurl = "http://www.lancers.jp"
-  val doc = Jsoup.connect("http://www.lancers.jp/work/search/system?completed=1&money_min=100&money_max=100000&sort=Work.budget_from&direction=asc&")
+  val doc = Jsoup.connect("http://www.lancers.jp/work/search/system/development?completed=0&money_max=100000&sort=Work.started&direction=desc")
       .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6").get()
 
   def main(args: Array[String]): Unit = {
-    val id = doc.getElementsByTag("p").attr("id").drop(5).toInt // id = work_877762 先頭5文字drop
+    var id = 0
     var new_project_url = ""
     var new_project_text = ""
     var post_text = ""
     var new_project_prices = ("", "")
-
-    if (id > max_id) {
-      new_project_url = getNewProjectUrl()
-      new_project_text = getNewProjectText(new_project_url)
-      new_project_prices = getProjectPrice(new_project_url)
-      post_text = new_project_url ++ "\n" ++ new_project_prices._1 ++ " ~ " ++ new_project_prices._2 ++ "\n" ++ new_project_text
-      if (!isContainingNG(post_text)) {
-        println(new_project_url)
-        println(new_project_text)
-        Slack.post(post_text)
+    while (true) {
+      id = doc.getElementsByTag("p").attr("id").drop(5).toInt // id = work_877762 先頭5文字drop
+      if (id > max_id) {
+        max_id = id
+        new_project_url = getNewProjectUrl()
+        new_project_text = getNewProjectText(new_project_url)
+        new_project_prices = getProjectPrice(new_project_url)
+        post_text = new_project_url ++ "\n" ++ new_project_prices._1 ++ " ~ " ++ new_project_prices._2 ++ "\n" ++ new_project_text
+        if (!isContainingNG(post_text)) {
+          println(new_project_url)
+          println(new_project_text)
+          Slack.post(id.toString ++ post_text)
+        } else {
+          println("containing NG")
+        }
+      } else {
+        println("nope " ++ max_id.toString)
       }
+      Thread.sleep(600000)
     }
   }
 
   def getNewProjectUrl(): String = {
-    val new_project_url = baseurl ++ doc.getElementsByClass("work_title").asScala.map(x => x.getElementsByTag("a").attr("href")).tail.tail.tail.tail.tail.tail.tail.tail.tail.tail.tail.head
+    val new_project_url = baseurl ++ doc.getElementsByClass("work_title").asScala.map(x => x.getElementsByTag("a").attr("href")).tail.head
     new_project_url
   }
 
@@ -63,13 +72,12 @@ object Main {
   }
 }
 
-import com.flyberrycapital.slack.SlackClient
 object Slack {
   val s = new SlackClient("xoxp-19896831492-19900049315-20237606272-31cff2a970")
   s.connTimeout(1000)
   s.readTimeout(5000)
   
   def post(new_project_url: String): Unit = {
-    s.chat.postMessage("#piimemo", new_project_url)
+    s.chat.postMessage("#lancers", new_project_url)
   }
 }
